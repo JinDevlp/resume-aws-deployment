2f8e601cac08bed743f862b3faa5e2e27525bcf3
Jin Choi <91814116+JinDevlp@users.noreply.github.com>
2022-12-27 19:58:41 -0600

    Update settings.py

--- portfolio_aws/portfoliowebsite/settings.py
+++ portfolio_aws/portfoliowebsite/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'PORT': 5432
     }
 }
-# localhost:8000 cool_snyder
+
 
 
 
@@ -130,8 +130,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
 
 
 STATIC_URL = '/static/'
@@ -139,23 +138,12 @@ STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
-# MEDIA_ROOT=os.path.join(BASE_DIR, 'static/images')
 
 
-# Before
-# STATIC_URL = '/static/'
-# # STATIC_ROOT=os.path.join(BASE_DIR,'static')
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
 
 
-# # MEDIA_URL = '/media/images/'
-# MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
 
 
-# STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 
 
